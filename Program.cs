b8d638818879e9244c01507458f16c033ff1061e
Muhammed Sahin <121553425+azulawashere@users.noreply.github.com>
2023-02-17 23:54:13 +0300

    addded doc

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+
+            string[] a = { };
+            for (int i = 0; true; i++)
+            {
+                a[i] = "sdlga";
+            }
+            
+
+   
